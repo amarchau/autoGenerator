a8adde155bcfe34dcdadf22bf0c2f91e5e09dc2f
Amar Chauhan <amar@amarchauhan.com>
2022-10-21 13:13:45 +0530

    corrected option randomiser and fixed zero answer

--- automation.py
+++ automation.py
@@ -1,7 +1,8 @@
 import random
 
+QuestionCount = 10
 Questions = list()
-QuestionCount = 5
+ques_tup = list()
 while QuestionCount>0:
     DoubleDigit = 1
     while DoubleDigit:
@@ -11,22 +12,30 @@ while QuestionCount>0:
         #print (a,'+',b, '=', c)
         if ans<10:
             Question = str(val1)+' + ' +str(val2) +' = '+ str(ans)
-            if Question in Questions:
+            tup = sorted((val1, val2))
+            if tup in ques_tup:
                 x=1
+                print(tup, "errrrorororo")
             else:
                 DoubleDigit = 0
                 print (Question)
                 Questions.append(Question)
+                ques_tup.append(tup)
                 a=ans
-                b,c,d = -1,-1,-1
-                while b < 1 and c < 1 and d < 1 :
-                    b=ans+random.randint(0,3)
-                    c=ans-random.randint(0,3)
-                    d=ans+random.randint(0,3)
+                #b,c,d = -1,-1,-1
+                #while b < 1 and c < 1 and d < 1 :
+                b=ans+random.randint(1,3)
+                d=ans+random.randint(1,3)
+                if ans==0:  #avoiding loop if ans is 0
+                    c=ans+random.randint(1,3)
+                else:
+                    c=ans-random.randint(1,3)
+                while c<0:  #avoiding negetive options
+                    c=ans-random.randint(1,3)
                     while d == a or d == b or d == c:
                         d=ans+random.randint(0,3)
-                    print ('a:',a,',b:',b,',c:',c,',d:',d )
-                    QuestionCount-= 1
-
-
-                    #hello india
+                options = [a, b, c ,d]
+                random.shuffle(options)
+                print (options)
+                #print ('a:',a,',b:',b,',c:',c,',d:',d )
+                QuestionCount-= 1
