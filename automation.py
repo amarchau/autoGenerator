791d341091d18763a5d7d74aad5ff940ccd7c963
Amar Chauhan <amar@amarchauhan.com>
2022-10-21 16:48:12 +0530

    added sensible option generator with reduced code

--- automation.py
+++ automation.py
@@ -1,13 +1,12 @@
 import random
 
 question_count = 10
-question_digits = 2
+question_digits = 5
 Questions = list()
 question_values = list()
 ques_tup = list()
 
-# if question_digits < 2 :
-#     question_digits = question_count
+
 while question_count>0:    
     while question_digits>0:
         DoubleDigit = 1
@@ -15,7 +14,6 @@ while question_count>0:
             val1 = random.randint(0,9)
             val2 = random.randint(0,9)
             ans = val1 + val2
-            #print (a,'+',b, '=', c)
             if 0<ans<10:
                 Question = str(val1)+' + ' +str(val2) +' = '+ str(ans)
                 tup = sorted((val1, val2))
@@ -24,29 +22,11 @@ while question_count>0:
                     print(tup, "errrrorororo")
                 else:
                     DoubleDigit = 0
-                    #print (Question)
                     Questions.append(Question)
                     ques_tup.append(tup)
-                    a=ans
-                    #b,c,d = -1,-1,-1
-                    #while b < 1 and c < 1 and d < 1 :
-                    b=ans+random.randint(1,3)
-                    c=ans-random.randint(1,3)
-                    d=ans+random.randint(1,3)
-                    # if ans==0:  #avoiding loop if ans is 0
-                    #     c=ans+random.randint(1,3)
-                    # else:
-                    while c<0:  #avoiding negetive options
-                        c=ans-random.randint(1,3)
-                        while d == a or d == b or d == c:
-                            d=ans+random.randint(0,3)
-                    options = [a, b, c ,d]
-                    if question_digits>1:
-                        random.shuffle(options)
-                    question_elements = ((val1, val2, ans, b, c ,d))
+                    a=ans                   
+                    question_elements = ((val1, val2, ans))
                     question_values.append(question_elements)
-                    #print (options)
-                    #print ('a:',a,',b:',b,',c:',c,',d:',d )
                     question_digits-= 1
     question_digits=len(question_values)
     #print (len(question_values))
@@ -60,12 +40,19 @@ while question_count>0:
         val11 += str(question_values[question_digits-1][0])
         val22=val22+str(question_values[question_digits-1][1])
         anss=anss+str(question_values[question_digits-1][2])
-        bb=bb+str(question_values[question_digits-1][3])
-        cc=cc+str(question_values[question_digits-1][4])
-        dd=dd+str(question_values[question_digits-1][5])
         question_digits-= 1
-    print (val11,'+',val22,'=',anss)
-    print (anss,',',bb,',',cc,',',dd)
+    answer=int(anss)
+    bb=answer+random.randint(1,3*len(question_values))
+    cc=answer-random.randint(1,3*len(question_values))
+    dd=answer+random.randint(1,3*len(question_values))
+    while cc<0:  #avoiding negetive options
+        cc=answer-random.randint(1,3)
+        while dd == answer or dd == bb or dd == cc:
+            dd=answer+random.randint(0,3)
+    options = [answer, bb, cc ,dd]
+    random.shuffle(options)
+    print (val11,'+',val22,'=',answer)
+    print (options)
     question_digits=len(question_values)
     question_values=[]
     ques_tup=[]
